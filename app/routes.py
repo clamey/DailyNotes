ba3c30a06ceff0132f00ac71e449c73675bc8e63
Topher Lamey <clamey@gmail.com>
2025-11-17 16:21:54 -0700

    Take out hidden path

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,6 @@ from app.models import User, Note, Meta, aes_encrypt, aes_encrypt_old
 from flask import render_template, request, jsonify, abort, send_file
 from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 from config import OTHER_BASE
-from not_hidden_config import ALL_YOUR_BASE
 
 @app.route('/api/sign-up', methods=['POST'])
 def sign_up():
@@ -400,11 +399,6 @@ def search():
   return jsonify(notes=sorted_nodes), 200
 
 BASE="/foo"
-
-@app.route(ALL_YOUR_BASE + '/<int:count>')
-@jwt_required()
-def all_your_foo_bar():
-    return "ok", 200
 
 @app.route(OTHER_BASE + '/<int:count>')
 @jwt_required()
